2755996b6ab3fafe3b6fd0e1c1fcbcc51388d53e
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-18 08:48:00 +0700

    Body layout

---
+++ src/components/posts/Post.js
@@ -0,0 +1,19 @@
+import React from "react";
+import birdLogo from "../../assets/blue-origami-bird.png";
+
+const Post = ({ description, author }) => {
+  return (
+    <div className="Post">
+      <img src={birdLogo} alt="logo" />
+      <p className="description">{description}</p>
+      <div>
+        <span>
+          <small>Author:</small>
+          {author}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Post from "../components/posts/Post";
 import Aside from "./Aside";
 
 const Main = () => {
+  const [posts, setPost] = useState([
+    {
+      id: 1,
+      description: "Desc 1",
+      author: "author 1",
+    },
+    {
+      id: 2,
+      description: "Desc 2",
+      author: "author 2",
+    },
+    {
+      id: 3,
+      description: "Desc 3",
+      author: "author 3",
+    },
+  ]);
   return (
     <div className="Container">
       <Aside />
       <div className="Main">
-        <h1>This is Main content</h1>
+        <div className="Posts">
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              description={post.description}
+              author={post.author}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
