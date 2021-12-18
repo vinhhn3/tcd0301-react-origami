b302ff00648b3205d9a75e0a377734e27eac8cd5
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-18 08:16:30 +0700

    Setup the web layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Aside from "./pages/Aside";
+import Footer from "./pages/Footer";
+import Main from "./pages/Main";
+import Navbar from "./pages/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <Aside />
+      <Main />
+      <Footer />
     </div>
   );
 }
